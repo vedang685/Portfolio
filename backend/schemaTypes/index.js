865615bc6b8b5487d0eaacdab34872b3a9cd715d
Vedang <vedang685@gmail.com>
2024-03-28 03:02:41 +0530

    Created Backend

---
+++ backend/schemaTypes/index.js
@@ -0,0 +1,10 @@
+import testimonials from "./testimonials"
+import works from "./works"
+import abouts from "./abouts"
+import brands from "./brands"
+import contact from "./contact"
+import experiences from "./experiences"
+import skills from "./skills"
+import workExperience from "./workExperience"
+
+export const schemaTypes = [works, testimonials, brands, abouts, skills, workExperience, experiences, contact]
---
+++ backend/schemaTypes/testimonials.js
@@ -0,0 +1,30 @@
+export default {
+    name: 'testimonials',
+    title: 'testimonials',
+    type: 'document',
+    fields: [
+        {
+            name: 'name',
+            title: 'Name',
+            type: 'string'
+        },
+        {
+            name: 'company',
+            title: 'Company',
+            type: 'string'
+        },
+        {
+            name: 'imageurl',
+            title: 'ImgURL',
+            type: 'image',
+            options: {
+                hotspot: true,
+            }
+        },
+        {
+            name: 'feedback',
+            title: 'Feedback',
+            type: 'string'
+        },
+    ]
+}

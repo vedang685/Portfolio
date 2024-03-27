f43edd7b03a0934939dce9ed1bc2623ff49d3174
Vedang <vedang685@gmail.com>
2024-03-28 04:33:31 +0530

    Added Assets and edited folder structure

---
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Navbar.scss'
+
+const Navbar = () => {
+  return (
+    <nav>
+        <div>
+            <img src="{images.logo}" alt="" />
+        </div>
+    </nav>
+  )
+}
+
+export default Navbar

06b0b2cd454607b85a3ef156e1059a7c69fb909b
Vedang <vedang685@gmail.com>
2024-03-28 14:17:18 +0530

    Navbar commit

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -4,10 +4,18 @@ import { images } from '../../constants'
 
 const Navbar = () => {
   return (
-    <nav>
-        <div>
+    <nav className='app__navbar'>
+        <div className='app__navbar-logo'>
             <img src={images.logo} alt="" />
         </div>
+        <ul className='app__navbar-links'>
+            {['home', 'about', 'work', 'skills', 'contact'].map((item)=>(
+                <li className="app__flex p-text" key={`link-${item}`}>
+                    <div></div>
+                    <a href={`#${item}`}>{item}</a>
+                </li>
+            ))}
+        </ul>
     </nav>
   )
 }

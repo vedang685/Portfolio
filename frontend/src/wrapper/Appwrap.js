bdbdae4532dc9e9097d93766823411095163919c
Vedang <vedang685@gmail.com>
2024-03-29 20:24:04 +0530

    updated work.jsx

--- frontend/src/wrapper/Appwrap.js
+++ frontend/src/wrapper/Appwrap.js
@@ -7,10 +7,10 @@ const Appwrap = (Component, idName, classNames) => function HOC() {
         <SocialMedia/>
         <div className='app__wrapper app__flex'>
             <Component/>
-            <div className='copyright'>
+            {/* <div className='copyright'>
                 <p className='p-text'>@2024 Vedang</p>
                 <p className='p-text'>All rights reserved</p>
-            </div>
+            </div> */}
         </div>
         <NavigationDots active={idName}/>
     </div>

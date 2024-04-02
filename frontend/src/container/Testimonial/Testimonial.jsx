22145417536ae605dd217b95f8071af33494ddfc
Vedang <vedang685@gmail.com>
2024-04-03 00:55:52 +0530

    Update testimonial.jsx and .scss

--- frontend/src/container/Testimonial/Testimonial.jsx
+++ frontend/src/container/Testimonial/Testimonial.jsx
@@ -24,14 +24,50 @@ const Testimonial = () => {
         setBrands(data);
       })
   },[])
+  const test = testimonials[currentIndex];
+  const handleClick = (index) => {
+    setcurrentIndex(index);
+  }
   return (
-    <>
+  <>
+    
     {testimonials.length && (
+      <>
       <div className='app__testimonials-item app__flex'>
-        <img src={urlFor(testimonials[currentIndex].imgUrl)} alt='testimonials'/>
+        <img src={urlFor(test.imageurl)} alt='testimonials'/>
+        <div className='app__testimonial-content'>
+          <p className='p-text'>
+            {test.feedback}
+          </p>
+          <div>
+            <h4 className='bold-text'>{test.name}</h4>
+            <h5 className='p-text'>{test.company}</h5>
+          </div>
+        </div>
       </div>
+      <div className='app__testimonial-btns app__flex'>
+        <div className='app__flex' onClick={()=> handleClick(currentIndex === 0? testimonials.length-1:currentIndex-1)}>
+          <HiChevronLeft/>
+        </div>
+        <div className='app__flex' onClick={()=> handleClick(currentIndex === testimonials.length-1? 0 :currentIndex+1)}>
+          <HiChevronRight/>
+        </div>
+      </div>
+      </>
     )}
-    </>
+    
+    <div className="app__testimonial-brands app__flex">
+      {brands.map((brand)=>(
+        <motion.div
+          whileInView={{opacity:[0,1]}}
+          transition={{duration: 0.5, type: 'tween'}}
+          key={brand._id}
+        >
+          <img src={urlFor(brand.imgUrl)} alt={brand.name}/>
+        </motion.div>
+      ))}
+    </div>
+  </>
   )
 }
 

14cca2978544bb3dd8e492ca115fb9077a052c1f
Vedang <vedang685@gmail.com>
2024-04-05 05:35:40 +0530

    update Footer.jsx

--- frontend/src/container/Footer/Footer.jsx
+++ frontend/src/container/Footer/Footer.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
+import {images} from '../../constants'
+import {Appwrap, MotionWrap} from '../../wrapper'
+import {client} from '../../client'
 import './Footer.scss'
 
 const Footer = () => {
   return (
-    <div>Footeree</div>
+    <>
+    <h2 className='head-text'>Take a coffee and chat with me</h2>
+    <div className='app__footer-cards'>
+      <div className='app__footer-card'>
+        <img src={images.email} alt='email'/>
+        <a href="mailto:vedang685@gmail.com" className='p-text'>vedang685@gmail.com</a>
+      </div>
+    </div>
+    </>
   )
 }
 
-export default Footer
+export default Appwrap(MotionWrap(Footer,'app__footer'),'contact','app__whitebg')

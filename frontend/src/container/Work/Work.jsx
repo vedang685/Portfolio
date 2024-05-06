bce3596e6ac20669ce3b4cff47336ab9e1c82725
Vedang <vedang685@gmail.com>
2024-05-07 02:09:00 +0530

    Updated card for mobile devices

--- frontend/src/container/Work/Work.jsx
+++ frontend/src/container/Work/Work.jsx
@@ -4,7 +4,9 @@ import {AiFillEye, AiFillGithub} from 'react-icons/ai'
 import {motion} from 'framer-motion'
 import {Appwrap, MotionWrap} from '../../wrapper'
 import {urlFor, client} from '../../client'
-
+import { useMediaQuery } from '@react-hook/media-query';
+import { FaGithub } from "react-icons/fa";
+import { FaGlobe } from "react-icons/fa";
 import './Work.scss'
 
 const Work = () => {
@@ -12,6 +14,9 @@ const Work = () => {
   const [animateCard, setAnimateCard] = useState({y:0, opacity:1})
   const [works, setWorks] = useState([]);
   const [filterWork, setFilterWork] = useState([])
+  const isLargeScreen = useMediaQuery('(min-width: 768px)')
+  const isSmallScreen = !isLargeScreen
+  
   useEffect(()=>{
     const query = '*[_type == "works"]';
     client.fetch(query)
@@ -44,7 +49,7 @@ const Work = () => {
       My creative <span>Portfolio</span> section
     </h2>
     <div className='app__work-filter'>
-      {['UI/UX','Web App','Mobile App','React JS','All'].map((item,index)=>(
+      {['UI/UX','Web App','React JS','All'].map((item,index)=>(
         <div
         key={index}
         onClick={()=>handleWorkFilter(item)}
@@ -65,36 +70,49 @@ const Work = () => {
           <div className='app__work-item app__flex' key={index}>
             <div className='app__work-img app__flex'>
               <img src={urlFor(work.imgUrl)} alt={work.name}/>
-              <motion.div
-                whileHover={{opacity: [0,1]}}
-                transition={{duration: 0.25, ease: 'easeInOut', staggerChildren: 0.5}}
-                className='app__work-hover app__flex'
-              >
-                <a href={work.projectLink} target='_blank' rel='noreferrer'>
-                  <motion.div
-                    whileInView={{scale:[0,1]}}
-                    whileHover={{scale: [1,0.9]}}
-                    transition={{duration: 0.25}}
-                    className='app__flex'
-                  >
-                    <AiFillEye/>
-                  </motion.div>
-                </a>
-                <a href={work.codeLink} target='_blank' rel='noreferrer'>
-                  <motion.div
-                    whileInView={{scale:[0,1]}}
-                    whileHover={{scale: [1,0.9]}}
-                    transition={{duration: 0.25}}
-                    className='app__flex'
-                  >
-                    <AiFillGithub/>
-                  </motion.div>
-                </a>
-              </motion.div>
+              {isLargeScreen && (
+                <motion.div
+                  whileHover={{opacity: [0,1]}}
+                  transition={{duration: 0.25, ease: 'easeInOut', staggerChildren: 0.5}}
+                  className='app__work-hover app__flex'
+                >
+                  <a href={work.projectLink} target='_blank' rel='noreferrer'>
+                    <motion.div
+                      whileInView={{scale:[0,1]}}
+                      whileHover={{scale: [1,0.9]}}
+                      transition={{duration: 0.25}}
+                      className='app__flex'
+                    >
+                      <AiFillEye/>
+                    </motion.div>
+                  </a>
+                  <a href={work.codeLink} target='_blank' rel='noreferrer'>
+                    <motion.div
+                      whileInView={{scale:[0,1]}}
+                      whileHover={{scale: [1,0.9]}}
+                      transition={{duration: 0.25}}
+                      className='app__flex'
+                    >
+                      <AiFillGithub/>
+                    </motion.div>
+                  </a>
+                </motion.div>
+              )}
             </div>
             <div className='app__work-content app__flex'>
               <h4 className='bold-text'>{work.title}</h4>
               <p className='p-text' style={{marginTop:10}}>{work.description}</p>
+              {console.log(work)}
+              {isSmallScreen && (
+                <div className='app__footer-link app__social'>
+                <a href= {work.codeLink}>
+                <FaGithub/>
+                </a>
+                <a href= {work.projectLink}>
+                <FaGlobe/>
+                </a>
+                </div>
+              )}
               <div className='app__work-tag app__flex'>
                 <p className='p-text'>{work.tags[0]}</p>
               </div>

5cdf23a19a6edae14435d23f300900a9cf68a133
Vedang <vedang685@gmail.com>
2024-05-09 00:50:58 +0530

    caseStudy Commit

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,11 +3,28 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {
+  createBrowserRouter,
+  RouterProvider
+} from 'react-router-dom'
+import CaseStudy from './container/CaseStudy/CaseStudy';
+import Page2 from './container/Page2/Page2';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App/>,
+  },
+  {
+    path:"case-study",
+    element: <Page2/>
+  }
+])
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>
 );
 reportWebVitals();

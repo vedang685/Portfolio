21db5a1e79b8f23cb0dc580002628b1013011a31
Vedang <vedang685@gmail.com>
2024-05-09 15:26:58 +0530

    Update MotionWrap.js

--- frontend/src/wrapper/MotionWrap.js
+++ frontend/src/wrapper/MotionWrap.js
@@ -1,15 +1,33 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import {motion} from 'framer-motion';
+import { useLocation } from 'react-router-dom';
 
 const MotionWrap = (Component, classNames) => function HOC(){
+  const location = useLocation();
+  const [cs, setCs] = useState(false)
+  useEffect(()=>{
+    if(location.pathname === '/case-study'){
+      setCs(true)
+    }
+  },[location])
   return (
-    <motion.div
-        whileInView={{y:[100,50,0], opacity:[0,0,1]}}
-        transition={{duration:0.5}}
-        className={`${classNames} app__flex`}
-    >
-        <Component/>
-    </motion.div>
+    <>
+      {cs?(
+        <div
+          className={`${classNames} app__flex`}
+        >
+          <Component/>
+      </div>
+      ):(
+      <motion.div
+          whileInView={{y:[100,50,0], opacity:[0,0,1]}}
+          transition={{duration:0.5}}
+          className={`${classNames} app__flex`}
+      >
+          <Component/>
+      </motion.div>
+      )}
+    </>
   )
 }
 
